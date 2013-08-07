31ba468a375721ee406de79ce2fc5a62ca181b3d
lcyper <lcyper@gmail.com>
2013-08-06 21:46:02 -0300

    arreglitos

--- progamando.js
+++ progamando.js
@@ -5,7 +5,7 @@
 //barquitos
 
 //agua
-//tamano=Number(prompt("ancho",""));
+//tamano=Number(prompt("ancho del tablero:",""));
 var tamano=10;
 var ancho_mapa= new Array(1,2,3,4,5,6,7,8,9,10);
 var alto_mapa= new Array('a','b','c','d','e','f','g','h','i','j');
@@ -19,6 +19,7 @@ for (var i = 0; i < ancho_mapa.length; i++) {
 */
 function toque(a,b){
 c=a+b;
+
 console.log(c)
 //alert(c);
 document.getElementById(c).style.backgroundColor="red";
