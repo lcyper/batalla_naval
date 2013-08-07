92bd9a9c5b53026b29edad6b9a0896bb46ed5b0a
lcyper <lcyper@gmail.com>
2013-08-06 22:11:43 -0300

    array q recorre la tabla

--- progamando.js
+++ progamando.js
@@ -8,7 +8,22 @@
 //tamano=Number(prompt("ancho del tablero:",""));
 var tamano=10;
 var ancho_mapa= new Array(1,2,3,4,5,6,7,8,9,10);
+//var ancho_mapa_b= new Array(10);
 var alto_mapa= new Array('a','b','c','d','e','f','g','h','i','j');
+//var alto_mapa_b= new Array(10)
+
+// esto recorre la tabla hay que darle buen uso
+var tablero= new Array();
+for (var i = 0; i < ancho_mapa.length; i++) {
+	for (var d = 0; d < alto_mapa.length; d++) {
+		//console.log(alto_mapa[i]+ancho_mapa[d])
+		ayb=alto_mapa[i]+ancho_mapa[d];
+		tablero[ayb,0]
+		console.log(tablero[ayb,0])
+	};
+	
+};
+
 /*
 for (var i = 0; i < ancho_mapa.length; i++) {
 	 console.log(ancho_mapa[i])
