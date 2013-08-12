4e726c7a8e93f6c4734f894c55185255b8d75f30
lcyper <lcyper@gmail.com>
2013-08-12 15:49:14 -0300

    mejorado las funcionalidades y css

--- progamando.js
+++ progamando.js
@@ -1,27 +1,41 @@
 // JavaScript Document
-
 //tablas
 
 //barquitos
 
 //agua
 //tamano=Number(prompt("ancho del tablero:",""));
-var tamano=10;
+//var tamano=10;
 var ancho_mapa= new Array(1,2,3,4,5,6,7,8,9,10);
 //var ancho_mapa_b= new Array(10);
 var alto_mapa= new Array('a','b','c','d','e','f','g','h','i','j');
 //var alto_mapa_b= new Array(10)
-
 // esto recorre la tabla hay que darle buen uso
-function reset(a,b){
 var tablero= new Array();
 for (var i = 0; i < ancho_mapa.length; i++)
  {
 	for (var d = 0; d < alto_mapa.length; d++) 
+	{ayb=alto_mapa[i]+ancho_mapa[d];
+	tablero[ayb]=0;}
+}
+
+function mapa(a,b){
+for (var i = 0; i < ancho_mapa.length; i++)
+ {
+	for (var d = 0; d < alto_mapa.length; d++) 
 	{
-		//console.log(alto_mapa[i]+ancho_mapa[d])
+		//console.log(alto_mapa[i]+ancho_mapa[d]);
 		ayb=alto_mapa[i]+ancho_mapa[d];
-		tablero[ayb]=0;
+		console.log(ayb);
+		if (tablero[ayb]!=1) {tablero[ayb]=0;};
+		if (a==reset) {
+			tablero[ayb]=0;
+			document.getElementById(ayb).style.background='#10A7FC';
+		}
+		if (a=='jugar') {
+			document.getElementById(ayb).style.background='#10A7FC';
+			if (tablero[ayb]==1) {tablero[ayb]=2;};
+		}
 		console.log(tablero[ayb])
 		}
 	}
@@ -35,15 +49,35 @@ for (var i = 0; i < ancho_mapa.length; i++) {
 	 console.log(alto_mapa[i])
 };
 */
+var color="#524D4D",c;
+//var id='';
 function toque(a,b){
 c=a+b;
-document.getElementById(c).style.backgroundColor="red";
-//console.log(c);
-//console.log(tablero[c]);
-if (tablero[c]==1) {
-		document.getElementById(c).style.backgroundColor="black";
+//id=c;
+document.getElementById(c).style.backgroundColor=color;
+	if (tablero[c]==0) {
+			document.getElementById(c).style.border='red 2px;'
+		};
+	if (tablero[c]==2) {
+		document.getElementById(c).style.background='#000;';
 	};
-tablero[c]=1;
+	if (tablero[c]==0) {tablero[c]=1;};
+	console.log(c);
+	console.log(tablero[c]);
+	//if (tablero[c]==2) {go()};
 }
+
+function reset(){}
+
+function barcos(){}
+
+function go(){ 
+	mapa('jugar')
+	/*if (tablero[c]==1) {
+			tablero[c]=2;
+			console.log(tablero[c]);}*/
+		};
+	
+	
 
 //tocado
